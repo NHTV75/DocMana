8b790d30a566441288ff268d29fc72b995ae6521
hoangtien <tien07022004@gmail.com>
2024-10-21 15:53:47 +0700

    Update foler Model

---
+++ QuanLyHoSoDuAn/Model/Department.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyHoSoDuAn.Model
+{
+    public class Department
+    {
+        public string DepartmentID { get; set; }
+        public string DepartmentName { get; set; }
+        public string ManagerID { get; set; }
+        public string Description { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
---
+++ QuanLyHoSoDuAn/Model/User_Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyHoSoDuAn.Model
+{
+    public class User_Department
+    {
+        public string UserID { get; set; }
+        public string DepartmentID { get; set; }
+        public string Role { get; set; }
+        public DateTime AssignedDate { get; set; }
+    }
+}
